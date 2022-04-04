501de35a37d13b7123e6bfdcea9d3d73468c459e
Milad Alkhamis <mi.alkhamis@pm.me>
2022-04-04 18:31:25 +0430

    adding config loader

--- authz/authz.py
+++ authz/authz.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from authz.config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config) # Load Config from environments variables
     return app
---
+++ authz/config/config.py
@@ -0,0 +1,9 @@
+from os import environ
+
+class Config:
+    ######################### global Configuration #########################
+    ENV = environ.get("TOYBOX_AYTHZ_ENV","production")
+    DEBUG = bool(int(environ.get("TOYBOX_AYTHZ_DEBUG","0")))
+    TESTING = bool(int(environ.get("TOYBOX_AYTHZ_TESTING","0")))
+    SECRET_KEY = environ.get("TOYBOX_AUTHZ_SECRET_KEY", "HARD_STRONG_SECRET_KEY")
+    JSONIFY_PRETTYPRINT_REGULAR = True
