37a800f9a7538d11036e88c43672cdb6a9e6080c
Milad Alkhamis <mi.alkhamis@pm.me>
2022-04-05 21:37:16 +0430

    adding user resource and user controller protype

---
+++ authz/controller/apiv1/user.py
@@ -0,0 +1,16 @@
+from authz.util import jsonify
+class UserController:
+    
+    def get_user_list():
+        return jsonify(status=501)
+    def crate_user():
+        return jsonify(status=501)
+    
+    def get_user(user_id):
+        return jsonify(status=501)
+    
+    def update_user(user_id):
+        return jsonify(status=501)
+    
+    def delete_user(user_id):
+        return jsonify(status=501)
--- authz/resource/apiv1/__init__.py
+++ authz/resource/apiv1/__init__.py
@@ -1,1 +1,16 @@
 from authz.authz import apiv1 as api  # refers to apiv1 in authz file
+from authz.resource.apiv1.user import UserResource
+
+api.add_resource(
+    UserResource,
+    "/users",
+    methods=["GET","POST"],
+    endpoint="users"
+)
+
+api.add_resource(
+    UserResource,
+    "/users/<user_id>", #can set <user_id>:string|int|uuid...
+    methods=["GET","PATCH","DELETE"],
+    endpoint="user"
+)
