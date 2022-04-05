ef6d8559c95c771ecc4d516a0254dc660bf5c4ba
Milad Alkhamis <mi.alkhamis@pm.me>
2022-04-05 21:35:05 +0430

    adding restful package and apiv1 prototype

--- authz/authz.py
+++ authz/authz.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_restful import Api
+
 from authz.config import Config
+
+apiv1_bp = Blueprint("apiv1", __name__, url_prefix = "/api/v1")
+apiv1 = Api(apiv1_bp)
+
+from authz import resource # must be located here, after apiv1
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) # Load Config from environments variables
+    app.register_blueprint(apiv1_bp)
     return app
---
+++ authz/resource/apiv1/__init__.py
@@ -0,0 +1,1 @@
+from authz.authz import apiv1 as api  # refers to apiv1 in authz file
