05389e639f4b2f1a2132aaf7769f73131c59c4a2
Milad Alkhamis <mi.alkhamis@pm.me>
2022-04-06 18:32:03 +0430

    adding sql-alchemy package

--- authz/authz.py
+++ authz/authz.py
@@ -1,7 +1,10 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
 
 from authz.config import Config
+db = SQLAlchemy()
 
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix = "/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -11,5 +14,6 @@ from authz import resource # must be located here, after apiv1
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) # Load Config from environments variables
+    db.init_app(app) # init SQLAlchemy Database object
     app.register_blueprint(apiv1_bp)
     return app
--- authz/config/config.py
+++ authz/config/config.py
@@ -7,3 +7,8 @@ class Config:
     TESTING = bool(int(environ.get("TOYBOX_AYTHZ_TESTING","0")))
     SECRET_KEY = environ.get("TOYBOX_AUTHZ_SECRET_KEY", "HARD_STRONG_SECRET_KEY")
     JSONIFY_PRETTYPRINT_REGULAR = True
+    
+    ######################### Database Configuration #########################
+    SQLALCHEMY_DATABASE_URI = environ.get("TOYBOY_AUTHZ_DATABASE_URI",None)
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
+    
