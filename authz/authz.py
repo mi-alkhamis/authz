d46e2449feb212f40d120487797bf7a925e62f4b
Milad Alkhamis <mi.alkhamis@pm.me>
2022-04-07 15:07:52 +0430

    adding adn initializing Flask Marshmallow package

--- authz/authz.py
+++ authz/authz.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 from authz.config import Config
 db = SQLAlchemy()
+ma = Marshmallow()
 
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix = "/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -15,5 +17,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) # Load Config from environments variables
     db.init_app(app) # init SQLAlchemy Database object
+    ma.init_app(app)
     app.register_blueprint(apiv1_bp)
     return app
