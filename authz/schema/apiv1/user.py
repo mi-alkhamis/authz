834d1162781d397b661de0438aa5ae6b9bf84a27
Milad Alkhamis <mi.alkhamis@pm.me>
2022-04-14 18:40:03 +0430

    add last_change_at to model and schema

--- authz/schema/apiv1/user.py
+++ authz/schema/apiv1/user.py
@@ -1,11 +1,13 @@
 from authz.authz import ma
 from authz.model import User
 
-class UserSchema(ma.SQLAlchemySchema): # Help me to write a schema according to my model for sqlalchemy
-    
+
+class UserSchema(
+    ma.SQLAlchemySchema
+):  # Help me to write a schema according to my model for sqlalchemy
     class Meta:
         model = User
-    
+
     id = ma.auto_field(dump_only=True)
     username = ma.auto_field()
     password = ma.auto_field(load_only=True)
@@ -14,7 +16,7 @@ class UserSchema(ma.SQLAlchemySchema): # Help me to write a schema according to 
     expires_at = ma.auto_field(dump_only=True)
     last_login_at = ma.auto_field(dump_only=True)
     last_active_at = ma.auto_field(dump_only=True)
+    last_change_at = ma.auto_field(dump_only=True)
     failed_auth_at = ma.auto_field(dump_only=True)
     failed_auth_count = ma.auto_field(dump_only=True)
     status = ma.auto_field()
-    
